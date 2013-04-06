457586fe429fc77df0ea5a197a3e571cb990f540
unknown <trevormillner@gmail.com>
2013-04-06 19:45:36 -0400

    First unit test, url mapping, dummy view, settings

--- lists/tests.py
+++ lists/tests.py
@@ -4,13 +4,18 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+from django.core.urlresolvers import resolve
 from django.test import TestCase
-
+from lists.views import home_page
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.assertEqual(1 + 1, 3)
+        self.assertEqual(1 + 1, 2)
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
--- lists/views.py
+++ lists/views.py
@@ -1,1 +1,4 @@
 # Create your views here.
+home_page = None #can't use this to resolve url bc it isn't a function (it isn't callable)
+def home_page():
+    pass
