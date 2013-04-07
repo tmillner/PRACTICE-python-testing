8bbf5e8231b2f04a19179b9a9a3f57ff626a10f5
unknown <trevormillner@gmail.com>
2013-04-06 21:17:15 -0400

    Basic view now contains html

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,9 @@ Replace this with more appropriate tests for your application.
 """
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from lists.views import home_page
+
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -19,3 +21,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith("<html>"))
+        self.assertIn("<title>To-Do lists</title>", response.content)
+        self.assertTrue(response.content.endswith("</html>"))
+        
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,21 @@
+####################ALL VERSIONS OF WRITING DO THE SAME THING##################333
+##import django
+##
+### Create your views here.
+##home_page = None #can't use this to resolve url bc it isn't a function (it isn't callable)
+##def home_page(request):
+##    return django.http.HttpResponse()
+
+##import django.http
+##
+### Create your views here.
+##home_page = None #can't use this to resolve url bc it isn't a function (it isn't callable)
+##def home_page(request):
+##    return django.http.HttpResponse()
+
+from django.http import HttpResponse
+
 # Create your views here.
 home_page = None #can't use this to resolve url bc it isn't a function (it isn't callable)
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
